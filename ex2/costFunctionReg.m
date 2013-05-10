1df459e72d42b0e0db363e6f3d385c4ead323f0b
David Sullivan <david@galileo.(none)>
2013-05-10 17:17:47 +0100

    No need for element wise

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -21,7 +21,7 @@ thetaAlt = [0; theta(2:end)];
 
 h = sigmoid(X * theta);
 
-J = (1/m) * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + (lambda / (2*m)) .* (thetaAlt' * thetaAlt);
+J = (1/m) * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + (lambda / (2*m)) * (thetaAlt' * thetaAlt);
 
 grad = (1/m) * ((h - y)' * X) + (lambda/m) * thetaAlt';
 
